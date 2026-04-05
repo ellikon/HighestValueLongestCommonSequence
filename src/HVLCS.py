db4bb9ce9c4844fee9702b06fbb3f95a85375241
ordinalsoul <55070893+ordinalsoul@users.noreply.github.com>
2026-04-05 17:36:52 -0400

    Added input and output file computed into the data folder

--- src/HVLCS.py
+++ src/HVLCS.py
@@ -1,21 +1,13 @@
 import sys
 
-values = {
-    "a": 2,
-    "b": 4,
-    "c": 5
-}
 
-A = "aacb"
-B = "caab"
 def HVLCS(A,B, values):
     M = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
 
     for i in range(1, len(A) + 1):
         for j in range(1, len(B) + 1):
             if A[i - 1] == B[j - 1]:
-                M[i][j] = max(values[A[i - 1]] + M[i - 1][j - 1],
-                              M[i - 1][j], M[i][j - 1])
+                M[i][j] = values[A[i - 1]] + M[i - 1][j - 1]
             else:
                 M[i][j] = max(M[i - 1][j], M[i][j - 1])
 
@@ -28,7 +20,7 @@ def find_sequence(A, B, values, M):
     i, j = len(A), len(B)
     longest_sequence = ""
     while i > 0 and j > 0:
-        if A[i - 1] == B[j - 1] and M[i][j] == values[A[i - 1]] + M[i - 1][j - 1]:
+        if A[i - 1] == B[j - 1]:
             longest_sequence = A[i - 1] + longest_sequence
             i -= 1
             j -= 1
