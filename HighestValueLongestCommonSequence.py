54fdf4763cf94fc6499ba61d1b63eaf96c197e86
ordinalsoul <55070893+ordinalsoul@users.noreply.github.com>
2026-04-05 15:58:25 -0400

    Made the algorithms into functions for easier access to calls

--- HighestValueLongestCommonSequence.py
+++ HighestValueLongestCommonSequence.py
@@ -6,31 +6,38 @@ values = {
 
 A = "aacb"
 B = "caab"
+def HVLCS(A,B, values):
+    M = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
 
-M = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            if A[i - 1] == B[j - 1]:
+                M[i][j] = max(values[A[i - 1]] + M[i - 1][j - 1],
+                              M[i - 1][j], M[i][j - 1])
+            else:
+                M[i][j] = max(M[i - 1][j], M[i][j - 1])
 
-for i in range(1, len(A) + 1):
-    for j in range(1, len(B) + 1):
+    return M, M[len(A)][len(B)]
+
+
+
+
+def find_sequence(A, B, values, M):
+    i, j = len(A), len(B)
+    longest_sequence = ""
+    while i > 0 and j > 0:
         if A[i - 1] == B[j - 1]:
-            M[i][j] = values[A[i - 1]] + M[i - 1][j - 1]
-        else:
-            M[i][j] = max(M[i - 1][j], M[i][j - 1])
+            longest_sequence = A[i - 1] + longest_sequence
+            i -= 1
+            j -= 1
+        elif M[i - 1][j] == M[i][j]:
+            i -= 1
+        elif M[i][j - 1] == M[i][j]:
+            j -= 1
 
-highest_value = M[len(A)][len(B)]
-
-longest_sequence = ""
-
-i, j = len(A), len(B)
-
-while i > 0 and j > 0:
-    if A[i - 1] == B[j - 1]:
-        longest_sequence = A[i - 1] + longest_sequence
-        i -= 1
-        j -= 1
-    elif M[i - 1][j] == M[i][j]:
-        i -= 1
-    elif M[i][j - 1] == M[i][j]:
-        j -= 1
+    return longest_sequence
+M, highest_value = HVLCS(A,B,values)
+longest_sequence = find_sequence(A,B,values, M)
 
 print(highest_value)
 print(longest_sequence)
