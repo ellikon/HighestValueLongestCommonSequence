6cb48d4db0800bbe49275bcbd1351d15e832b9b7
ellikon <robbie@millertribe.com>
2026-04-05 15:44:08 -0400

    Added algorithm to find exact sequence from table

--- HighestValueLongestCommonSequence.py
+++ HighestValueLongestCommonSequence.py
@@ -18,7 +18,19 @@ for i in range(1, len(A) + 1):
 
 highest_value = M[len(A)][len(B)]
 
-longest_sequence = " " * highest_value
+longest_sequence = ""
 
-print(M)
+i, j = len(A), len(B)
+
+while i > 0 and j > 0:
+    if A[i - 1] == B[j - 1]:
+        longest_sequence = A[i - 1] + longest_sequence
+        i -= 1
+        j -= 1
+    elif M[i - 1][j] == M[i][j]:
+        i -= 1
+    elif M[i][j - 1] == M[i][j]:
+        j -= 1
+
 print(highest_value)
+print(longest_sequence)
